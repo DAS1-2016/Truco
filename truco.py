4eb6142f5438ada77302e8a7044f1288a596d88b
Italo Paiva <italosensei@hotmail.com>
2016-06-07 09:25:44 -0300

    Issue #1 - Finishing deck pool

--- truco.py
+++ truco.py
@@ -1,7 +1,7 @@
-
+import random
 
 class Card():
-    """Class for Card"""
+    """Prototype class for Card"""
     def __init__(self, suit, value=None):
         self.suit = suit
         self.value = value
@@ -9,9 +9,24 @@ class Card():
     def clone(self, value):
         return Card(self.suit, value)
 
-class Deck():
-    """Class for Deck"""
+    def __str__(self):
+        suit = self.suit
+        value = self.value
+        if value is "A":
+            value = "As"
+        elif value is "K":
+            value = "Rei"
+        elif value is "Q":
+            value = "Dama"
+        elif value is "J":
+            value = "Valete"
 
+        return value + " de " + suit
+
+class Deck():
+    """Singleton Pool class for Deck"""
+
+    CARDS_QUANTITY = 40
     instance = None
     
     @staticmethod
@@ -25,12 +40,12 @@ class Deck():
         self.__init_deck()
 
     def __init_deck(self):
-        self.build_cards("Copas")
-        self.build_cards("Ouros")
-        self.build_cards("Espadas")
-        self.build_cards("Paus")
+        self.__create_cards("Copas")
+        self.__create_cards("Ouros")
+        self.__create_cards("Espadas")
+        self.__create_cards("Paus")
     
-    def build_cards(self, suit):
+    def __create_cards(self, suit):
         suit_card = Card(suit)
         self.cards.append(suit_card.clone("A"))
         self.cards.append(suit_card.clone("J"))
@@ -41,24 +56,42 @@ class Deck():
             card = suit_card.clone(value)
             self.cards.append(card)
 
+    def shuffle(self):
+        self.__check_deck()
+        random.shuffle(self.cards)
 
-class ClassName(object):
-    """docstring for ClassName"""
-    def __init__(self, arg):
-        super(ClassName, self).__init__()
-        self.arg = arg
-        
+    def __check_deck(self):
+        """ Check deck integrity """
+        if len(self.cards) is not self.CARDS_QUANTITY:
+            raise Exception("Alguem esta roubando e nao devolveu todas as cartas!")
+
+    def get_top_card(self):
+        top_card = self.cards[0]
+        # Remove it from pool
+        self.cards.remove(top_card)
+        return top_card
+
+    def get_bottom_card(self):
+        bottom_card = self.cards[len(self.cards) - 1]
+        # Remove it from pool
+        self.cards.remove(bottom_card)
+        return bottom_card
+
+    def keep_card(self, card):
+        # Add the card back to the pool
+        self.cards.append(card)
+
 
 if __name__ == '__main__':
-  # cardCopas = Card("Copas")
-  # card = cardCopas.clone("2")
-  # print "Card 1"
-  # print card.suit
-  # print card.value
-  # card2 = cardCopas.clone("3")
-  # print "Card 2"
-  # print card2.suit
-  # print card2.value
-  deck = Deck.get_instance()
-  for card in deck.cards:
-    print card.suit + " " + card.value
+    deck = Deck.get_instance()
+    try:
+        deck.shuffle()
+    except Exception, e:
+        print e
+    card = deck.get_bottom_card()
+    print card
+    deck.keep_card(card)
+    try:
+        deck.shuffle()
+    except Exception, e:
+        print e
